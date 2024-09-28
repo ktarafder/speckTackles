9840b99507ca9e7022c09cb5775147754715e21c
Carbinski <carsonmcneill2004@gmail.com>
2024-09-28 16:57:59 -0400

    3 button different API Calls Update
    Look at 3 button api call update. The buttons now are stacked vertically and have labels that change colors to show what status it is.

--- ThreeButtonsDifferentAPICalls/Assets/setDataThree.js
+++ ThreeButtonsDifferentAPICalls/Assets/setDataThree.js
@@ -1,16 +1,29 @@
 //@input Asset.RemoteServiceModule rsm
+//@input Component.Text textComponent
 
+lightOn = false;
 const request = RemoteServiceHttpRequest.create();
 request.url = "https://spectaclesweb.vercel.app/api";
 request.method = RemoteServiceHttpRequest.HttpRequestMethod.Get;
 //script.rsm.performHttpRequest(request, onResponse);
+
+
 function onResponse(response){
     print("Status code: " + response.statusCode);
     print(response.body);
+    
+    if (response.statusCode === 200) {
+        if (lightOn) {
+            script.textComponent.textFill.color = new vec4(.70, .70, .70, 1);
+        } else {
+            script.textComponent.textFill.color = new vec4(1, .98, .474, 1);
+        }
+        lightOn = !lightOn;
+    }
 }
 
 function getResponse(){
-    print("Third button toggled!");
+    print("fan light!");
     script.rsm.performHttpRequest(request, onResponse);
 }
 
