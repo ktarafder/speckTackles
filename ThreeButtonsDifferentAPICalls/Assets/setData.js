a2e9a736c9fe9febb786b624ed122c8ff743c808
Carbinski <carsonmcneill2004@gmail.com>
2024-09-28 14:02:47 -0400

    Added new button calls
    Use the ThreeDifferentButton calls to test. The Logger should show that a different function is being called for each API. ie: First button toggled ... etc.

---
+++ ThreeButtonsDifferentAPICalls/Assets/setData.js
@@ -0,0 +1,18 @@
+//@input Asset.RemoteServiceModule rsm
+
+const request = RemoteServiceHttpRequest.create();
+request.url = "https://spectaclesweb.vercel.app/api";
+request.method = RemoteServiceHttpRequest.HttpRequestMethod.Get;
+//script.rsm.performHttpRequest(request, onResponse);
+function onResponse(response){
+    print("Status code: " + response.statusCode);
+    print(response.body);
+}
+
+function getResponse(){
+    print("First button toggled!");
+    script.rsm.performHttpRequest(request, onResponse);
+}
+
+script.getResponse = getResponse;
+
